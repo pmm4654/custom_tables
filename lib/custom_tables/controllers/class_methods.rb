64680f66d852db86a8db0e2ab0a9594cd86790aa
Patrick MacMurchy <pmm4654@gmail.com>
2018-04-07 08:11:36 -0400

    Use def instead of define_method

--- lib/custom_tables/controllers/class_methods.rb
+++ lib/custom_tables/controllers/class_methods.rb
@@ -7,16 +7,24 @@ module CustomTables
         send(:include, InstanceMethods)
         @index_headers = config[:default_headers]
         before_action :set_default_headers, :only => [:index] + config.fetch(:additional_before_actions, [])
-        define_method "set_default_headers" do
-          resource_class = params[:controller].classify.underscore
-          @default_headers = config.fetch(:default_headers, [])
-          @translation_base = config.fetch(:translation_base, resource_class)
-        end
+        @default_headers = config.fetch(:default_headers, [])
+        @translation_base = config.fetch(:translation_base, nil)
       end
 
+      def default_headers
+        @default_headers
+      end
+
+      def translation_base
+        @translation_base
+      end
 
       module InstanceMethods
-
+        def set_default_headers
+          resource_class = params[:controller].classify.underscore
+          @default_headers = self.class.default_headers
+          @translation_base = self.class.translation_base || resource_class
+        end
       end
 
     end
